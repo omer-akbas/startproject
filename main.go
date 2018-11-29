b1946cfb44c8ce28671dae248074a4d662565f45
omer akbas <omerakbas@gmail.com>
2018-11-29 12:17:58 +0300

    random token

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello World.")
+	fmt.Println(randToken(3))
+}
+
+func randToken(len int) string {
+	b := make([]byte, len)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
 }
